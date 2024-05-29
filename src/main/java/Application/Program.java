31cd3ff3c8ed9f59d453b60c583e6c67bc6d894e
Mauros Milach Junior <mauros99milach@gmail.com>
2024-05-29 11:30:44 -0300

    feat: completed JPA mapping, performed code review, and tests

--- src/main/java/Application/Program.java
+++ src/main/java/Application/Program.java
@@ -1,18 +1,44 @@
 package Application;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import domain.Pessoa;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Pessoa pessoa1 = new Pessoa (1, "Carlos Silva", "carlos.silva@email.com");
-		Pessoa pessoa2 = new Pessoa (2, "Joaquim Torres", "joaquim.torres@email.com");
-		Pessoa pessoa3 = new Pessoa (3, "Ana Maria", "ana.maria@email.com");
+		Pessoa pessoa1 = new Pessoa (null, "Carlos Silva", "carlos.silva@email.com");
+		Pessoa pessoa2 = new Pessoa (null, "Joaquim Torres", "joaquim.torres@email.com");
+		Pessoa pessoa3 = new Pessoa (null, "Ana Maria", "ana.maria@email.com");
 		
-		System.out.println(pessoa1);
-		System.out.println(pessoa2);
-		System.out.println(pessoa3);
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		
+		/*
+		 * Create Row (Objects)
+		 * 
+		em.persist(pessoa1);
+		em.persist(pessoa2);
+		em.persist(pessoa3);
+		
+		System.out.println("Pronto");
+		*/
+		
+		//Consulta
+		Pessoa p = em.find(Pessoa.class, 2);
+		System.out.println(p);
+		
+		//Remove 
+		em.remove(p);
+		
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
 		
 	}
 
